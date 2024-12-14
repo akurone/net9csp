c22dda98b4d2bf59a1f7f1804612b7eeaa313017
Ahmet Kuru <ahm@kuru.biz.tr>
2024-12-14 17:01:47 +0300

    initial: no csp, working

---
+++ net9csp/Program.cs
@@ -0,0 +1,30 @@
+using net9csp.Client.Pages;
+using net9csp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(net9csp.Client._Imports).Assembly);
+
+app.Run();
